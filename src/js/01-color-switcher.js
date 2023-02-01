0453fa03b750fff72e06e60cca4eca099407152b
MaksymKonin <skam707@gmail.com>
2023-02-01 16:42:01 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyEl = document.body;
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+const TIME_DELAY = 1000;
+let intervalId = null;
 
+btnStartEl.addEventListener('click', onTargetButtonStartClick);
+btnStopEl.addEventListener('click', onTargetButtonStopClick);
+
+function onTargetButtonStartClick(evt) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, TIME_DELAY);
+  btnStartEl.disabled = true;
+  btnStopEl.disabled = false;
+}
+
+function onTargetButtonStopClick(evt) {
+  clearInterval(intervalId);
+  btnStartEl.disabled = false;
+  btnStopEl.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
